69fcee2e2feda1a5d9ef94c9cdfe86ada20a5c45
ksihun <58981473+ksihun@users.noreply.github.com>
2025-11-16 14:38:08 +0900

    에임 분리/피격 이펙트 적용
    .

--- BlockOverflow/Assets/01.Scripts/Health/PlayerHealth.cs
+++ BlockOverflow/Assets/01.Scripts/Health/PlayerHealth.cs
@@ -1,21 +1,31 @@
 using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     
     [SerializeField] private float maxHealth = 100f;
     public float MaxHealth => maxHealth;
-    
     [ReadOnly, ShowInInspector] public float CurrentHealth{get; private set;}
     public bool IsDead { get; private set; }
+    
+    [SerializeField] private SpriteRenderer sr;
+    private Color originalColor;
+    
+    
 
     private void Awake()
     {
         //gameObject.SetActive(true);
         CurrentHealth = maxHealth;
         IsDead = false;
+        if (sr != null)
+        {
+            originalColor = sr.color;
+            //sr.color = Color.green;
+        }
     }
 
 
@@ -24,6 +34,9 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         if (IsDead) return;
     
         CurrentHealth = Mathf.Max(0, CurrentHealth - damageAmount);
+        // 혹시 전에 돌던 코루틴 있으면 끊고 다시
+        //StopCoroutine(nameof(HitFlash));
+        StartCoroutine(HitFlash());
         if (CurrentHealth <= 0f)
         {
             CurrentHealth = 0f;
@@ -54,4 +67,20 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         CurrentHealth = maxHealth;
         gameObject.SetActive(true);
     }
+    
+    private IEnumerator HitFlash()
+    {
+        if (sr == null)
+        {
+            Debug.LogWarning("HitFlash: sr가 null 입니다!", this);
+            yield break;
+        }
+        sr.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        sr.color = originalColor;
+        
+    }
+    
+    
+    
 }
