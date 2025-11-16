4149c15d371676359199587b799a681f3e972363
ksihun <58981473+ksihun@users.noreply.github.com>
2025-11-16 16:48:04 +0900

    die
    die애니메이션

--- BlockOverflow/Assets/01.Scripts/Weapons/WeaponController.cs
+++ BlockOverflow/Assets/01.Scripts/Weapons/WeaponController.cs
@@ -3,17 +3,22 @@ using UnityEngine.InputSystem;
 
 public class WeaponController : MonoBehaviour {
     [SerializeField] private Weapon weapon;
+    private PlayerInput _playerInput;
+    private InputAction _shootAction;
 
     private void Awake() {
         if (weapon == null) weapon = GetComponentInChildren<Weapon>();
+        _playerInput = GetComponentInParent<PlayerInput>();
+        _shootAction = _playerInput.actions?.FindAction("Attack");
         weapon?.Init();
     }
 
     private void Update() {
         if (weapon == null) return;
-        if (Mouse.current != null && Mouse.current.leftButton.isPressed) {
-            weapon.Fire();
-        }
+        // if (Mouse.current != null && Mouse.current.leftButton.isPressed) {
+        //     weapon.Fire();
+        // }
+        
     }
 
     public void SetWeapon(Weapon newWeapon) {
