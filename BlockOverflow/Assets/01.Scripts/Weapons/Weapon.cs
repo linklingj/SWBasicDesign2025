ef91e503fdd8dff571571e5940effe103cb311bd
최재현 <jaehyunc413@gmail.com>
2025-12-07 21:32:26 +0900

    이상한짓 하지마라

--- BlockOverflow/Assets/01.Scripts/Weapons/Weapon.cs
+++ BlockOverflow/Assets/01.Scripts/Weapons/Weapon.cs
@@ -121,18 +121,7 @@ public class Weapon : MonoBehaviour
 
         if (direction.sqrMagnitude <= Mathf.Epsilon) return false;
 
-        if (weaponData.name == "Pistol")
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                // 피스톨은 약간 랜덤 탄퍼짐
-                float spreadAngle = UnityEngine.Random.Range(-5f, 5f);
-                direction = Quaternion.Euler(0f, 0f, spreadAngle) * direction;
-                ShootBullet(spawnPos, direction, damageMultiplier);
-            }
-            
-        }
-        else ShootBullet(spawnPos, direction, damageMultiplier);
+        ShootBullet(spawnPos, direction, damageMultiplier);
         ScheduleNextShot();
         
         // 사운드 재생
