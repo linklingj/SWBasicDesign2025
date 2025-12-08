f5c0fb143476166cdf0aba8b7bb715e72f6e8e0b
Dongttak <22011789@sju.ac.kr>
2025-12-08 20:49:48 +0900

    Revert "눈 색 수정"
    This reverts commit bbffc29854122dcaa0650e3d99198b18f5ff650e.

--- BlockOverflow/Assets/01.Scripts/Managers/PlayerCustomize.cs
+++ BlockOverflow/Assets/01.Scripts/Managers/PlayerCustomize.cs
@@ -5,7 +5,6 @@ public class PlayerCustomize : MonoBehaviour
 {
     PlayerCustomization playerCustomization;
     [SerializeField] private SpriteRenderer playerBody;
-    [SerializeField] private SpriteRenderer eye;
     [SerializeField] private SpriteRenderer hat;
 
     public void Init(PlayerCustomization playerCustomization)
@@ -24,19 +23,16 @@ public class PlayerCustomize : MonoBehaviour
         if (playerCustomization == null)
         {
             playerBody.color = Color.white;
-            eye.color = Color.white;
             return;
         }
         
         if (tween)
         {
             playerBody.DOColor(playerCustomization.playerColor, 1f);
-            eye.DOColor(playerCustomization.playerColor, 1f);
         }
         else
         {
             playerBody.color = playerCustomization.playerColor;
-            eye.color = playerCustomization.playerColor;
         }
     }
     
