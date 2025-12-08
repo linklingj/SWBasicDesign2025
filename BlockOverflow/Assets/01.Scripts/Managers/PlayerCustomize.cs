bbffc29854122dcaa0650e3d99198b18f5ff650e
최재현 <jaehyunc413@gmail.com>
2025-12-08 20:38:31 +0900

    눈 색 수정

--- BlockOverflow/Assets/01.Scripts/Managers/PlayerCustomize.cs
+++ BlockOverflow/Assets/01.Scripts/Managers/PlayerCustomize.cs
@@ -5,6 +5,7 @@ public class PlayerCustomize : MonoBehaviour
 {
     PlayerCustomization playerCustomization;
     [SerializeField] private SpriteRenderer playerBody;
+    [SerializeField] private SpriteRenderer eye;
     [SerializeField] private SpriteRenderer hat;
 
     public void Init(PlayerCustomization playerCustomization)
@@ -23,16 +24,19 @@ public class PlayerCustomize : MonoBehaviour
         if (playerCustomization == null)
         {
             playerBody.color = Color.white;
+            eye.color = Color.white;
             return;
         }
         
         if (tween)
         {
             playerBody.DOColor(playerCustomization.playerColor, 1f);
+            eye.DOColor(playerCustomization.playerColor, 1f);
         }
         else
         {
             playerBody.color = playerCustomization.playerColor;
+            eye.color = playerCustomization.playerColor;
         }
     }
     
